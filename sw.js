83180bbbf1d6af1d2290592335f8b75844421833
harmanius-21 <harmanius@gmail.com>
2026-09-17 08:20:27 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE='geschiedenis-v35';
-const FILES=['./','./index.html','./style.css','./config.js?v=35','./questions.js?v=35','./app.js?v=35','./manifest.json','./logo.svg','./icon-192.png','./icon-512.png'];
+const CACHE='geschiedenis-v36';
+const FILES=['./','./index.html','./style.css','./config.js?v=36','./questions.js?v=36','./app.js?v=36','./manifest.json','./logo.svg','./icon-192.png','./icon-512.png'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(FILES)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
 self.addEventListener('fetch',e=>e.respondWith(fetch(e.request).then(r=>{const c=r.clone();caches.open(CACHE).then(x=>x.put(e.request,c)).catch(()=>{});return r}).catch(()=>caches.match(e.request))));
